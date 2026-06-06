38ff65d50e99885d2ba1c19a7a601e56e2c47cee
𝚂𝙰𝚁𝙰𝚅𝙰𝙽𝙰𝙽.𝙳 <saravananbass12@gmail.com>
2026-06-06 21:03:05 +0530

    Add files via upload

---
+++ SARAVANAN.py
@@ -0,0 +1,272 @@
+# TASK 1
+
+list=[10,20,30,40,50]
+if 20 in list: 
+    print("persent")
+else:
+    print("not persent")
+
+
+# TASK 2
+
+list=[10,20,30,40,50]
+if 100 in list: 
+    print("persent")
+else:
+    print("not persent")
+    
+
+# TASK 3
+
+char='p','y','t','h','o','n'
+if 'p' in char:
+    print("persent")
+else:
+    print("not persent")
+    
+
+# TASK 4
+
+char='p','y','t','h','o','n'
+if 'z' in char:
+    print("persent")
+else:
+    print("not persent")
+
+
+# TASK 5
+
+fruit='apple','banana','orange'
+if 'apple' in fruit:
+    print("apple is exists")
+else:
+    print("not exists")
+    
+
+# TASK 6
+
+for i in range(1,11):
+    print(i)
+    
+# TASK 7
+
+for i in range(5,16):
+    print(i)
+
+# TASK 8
+
+for i in range(2,21,2):
+    print(i)
+    
+# TASK 9
+
+for i in range(1,21,2):
+    print(i)
+  
+# TASK 10
+
+for i in range(10,101,10):
+    print(i)
+    
+# TASK 11
+
+for i in range(5,101,5):
+    print(i)
+    
+# TASK 12
+
+user=int(input("enter the value :"))
+for i in range(user, (user*10) +1,user):
+    print(i)
+
+# TASK 13
+
+sum=0
+for i in range(1,11):
+    sum=sum+i
+    print("sum :",sum)
+
+# TASK 14
+
+sum=0
+for i in range(2,21,2):
+   sum=sum+i
+   print("sum :",sum)
+
+# TASK 15
+
+for i in range(1,10):
+    print(i*i)
+
+
+# TASK 16
+
+for i in range(10,0,-1):
+    print(i)
+    
+
+# TASK 17
+
+for i in range(20,0,-1):
+    print(i)
+    
+
+# TASK 18
+
+for i in range(20,1,-2):
+    print(i)
+
+# TASK 19
+
+for i in range(19,1,-2):
+    print(i)
+    
+# TASK 20
+
+count=1
+for i in range(10,0,-1):
+    count=count+1
+    print(count)
+
+    
+# TASK 21
+
+for i in range(50,0,-5):
+    print(i)
+
+# TASK 22
+
+n=int(input("enter the value :"))
+for i in range(n,0,-1):
+    print(i)
+
+    
+# TASK 23
+
+for i in range(50,1,-5):
+    print(i)
+
+
+# TASK 24
+
+char="python"
+for i in char:
+    print(i,end=" ")
+
+    
+# TASK 25
+
+name="saravanan"
+for i in name:
+    print(i,end=" ")
+    
+# TASK 26
+
+n=input("enter the string :")
+count=0
+for i in n:
+    count+=1
+print("characters :",count)
+
+
+# TASK 27
+
+n=input("enter the string :")
+vowels=0
+for i in n:
+    vowels+=1
+print("vowels :",vowels)
+
+
+# TASK  28
+
+for i in range(5):
+    for j in range(5):
+        print(5,end=" ")
+    print()
+    
+
+# TASK  29
+
+for i in range (0,5):
+    for j in range(i+1):
+        print(j,end=" ")
+    print()
+
+
+# TASK  30
+
+for i in range (0,5):
+    for j in range(i,5):
+        print(j,end=" ")
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
